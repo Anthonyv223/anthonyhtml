16a63b429257d496ff6958c60448b72810009195
*Anthonyv223* <*avillalobos1@pride.hofstra.edu>
2025-03-25 22:53:31 -0400

    assignment6

---
+++ assignment6.0/script.js
@@ -0,0 +1,15 @@
+// Display a message in the browser console
+console.log("CSS + JavaScript is powerful!");
+
+// Show an alert message when the page loads
+window.onload = function () {
+    alert("Welcome to Assignment 5 with JavaScript!");
+};
+
+// Function to toggle between light mode and dark mode
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+
+    // Display a message in the console when the button is clicked
+    console.log("Theme changed!");
+}
